38520a8578686edf3bc5372d10ba732cefc1896e
innofarah <farahalwardany2012@gmail.com>
2022-02-13 20:26:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,35 +7,35 @@ fi
 
 
 # to install the latest stable node version through nvm, first install nvm:
-sudo apt-get install curl
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+apt-get install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 source ~/.bashrc
 source ~/.nvm/nvm.sh
 # install node latest stable version:
-sudo nvm install --lts
-sudo nvm use stable
+nvm install --lts
+nvm use stable
 # install ipfs (according to the ipfs website instructionS):
 cd ..
-sudo apt-get install wget
-sudo wget https://dist.ipfs.io/go-ipfs/v0.11.0/go-ipfs_v0.11.0_linux-amd64.tar.gz
-sudo tar -xvzf go-ipfs_v0.11.0_linux-amd64.tar.gz
+apt-get install wget
+wget https://dist.ipfs.io/go-ipfs/v0.11.0/go-ipfs_v0.11.0_linux-amd64.tar.gz
+tar -xvzf go-ipfs_v0.11.0_linux-amd64.tar.gz
 cd go-ipfs
-sudo bash install.sh
+bash install.sh
 ipfs init
 # to install w3proof-dispatch
-sudo apt install npm
+apt install npm
 #cleaning
-sudo npm uninstall w3proof-dispatch -g
-sudo nvm use system
-sudo npm uninstall -g a_module
-sudo nvm use stable
+npm uninstall w3proof-dispatch -g
+nvm use system
+npm uninstall -g a_module
+nvm use stable
 source ~/.bashrc
 source ~/.nvm/nvm.sh
 ## installing 
 cd ..
 cd w3proof-dispatch
-sudo npm i -g
-sudo npm ci
+npm i -g
+npm ci
 
 echo "
 ----------------------------------------------------------------------------------------------------------------------------
