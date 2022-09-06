4f47592de2d71249071f2993b4ae3a425cd43352
innofarah <farahalwardany2012@gmail.com>
2022-09-06 16:02:43 +0200

    index => more compact + -help

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -9,33 +9,44 @@ const { getCommand } = require('./get.js')
 setup()
 
 program
-    .command('keygen <profileName>')
-    .description('generate a PPK pair.')
+    .command('keygen')
+    .description('generate a PPK pair.\n')
+    .argument('<profile-name>', 'name for the key pair.\n')
     .action(keygen)
 program
-    .command('set-web3token <token>')
-    .description('set your web3.storage api token.')
+    .command('set-web3token')
+    .description('set your web3.storage api token.\n')
+    .argument('<token>', 'to use for publishing through the web3.storage api.\n' 
+    + 'you can create one at web3.storage website.\n')
     .action(setweb3token)
 
 program
-    .command('set-gateway <gateway>')
+    .command('set-gateway')
     .description('set your gateway.\n')
+    .argument('<gateway>', 'preferred default ipfs gateway.\n'
+    + 'the default is set to "https://dweb.link".\n')
     .action(setgateway)
 
 program
     .command('list-config')
-    .description('list config params.')
+    .description('list config params.\n')
     .action(listconfig)
 
 program
-    .command('publish-signed <fileName> <profileName> <directoryPath> <target>')
-    .description('publish-sign standard format sequents file.')
+    .command('publish-signed')
+    .description('publish-sign standard format sequents file.\n')
+    .argument('<file-name>', 'name of the file without the .json extension.\n')
+    .argument('<profile-name>', 'name of the profile used for signing each sequent.\n')
+    .argument('<directory-path>', 'path of directory containing the file starting from directory of execution.\n')
+    .argument('<target>', 'takes values: "local"->pin locally or "cloud"->pin through web3.storage.\n')
     .action(publishCommand)
 
 
 program
-    .command('get <CID> <directoryPath>')
-    .description('construct standard format output-to-prover file.')
+    .command('get')
+    .description('construct standard format input-to-prover file.\n')
+    .argument('<CID>', 'ipfs content identifier for the structure to get.\n')
+    .argument('<directory-path', 'container directory starting from directory of execution.\n')
     .action(getCommand)
 
 
